b54810a0cf623454781c8be721bfd8cadd99630a
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2022-08-25 10:07:20 +0100

    Routes for Post, getOne, getAll and delete fiixed for Books

--- books/books.js
+++ books/books.js
@@ -4,8 +4,12 @@ const app = express();
 const mongoose = require('mongoose');
 const env = require('dotenv');
 const bodyParser = require('body-parser');
+const { ppid } = require('process');
 // import 'dotenv/config'
+app.use(bodyParser.json());
 
+require("./Book")
+const Book = mongoose.model("Book")
 //CONNECT TO DATABASE
 mongoose.connect("mongodb+srv://microservice:abc789@cluster0.kmxki6p.mongodb.net/?retryWrites=true&w=majority", ()=> {
    console.log("Database is connected!");
@@ -17,8 +21,64 @@ app.get('/', (req, res)=> {
 });
 
 app.post('/book', (req, res) => {
-   res.send('Testing our book route')
+   var newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      numberPages: req.body.numberPages,
+      category: req.body.category
+   }
+   var book = new Book(newBook)
+   book.save().then(() => {
+      console.log('New book created!')
+   }).catch((err) => {
+      if (err){
+         throw err;
+      }
+   })
+   res.send('A new book created successfully!')
+   }
+)
+// Get all books
+app.get('/books', (req, res) => {
+   Book.find().then((books) => {
+      res.json(books)
+   }).catch(err => {
+      if (err){
+         throw err;
+      }
+   })
 })
+
+// get a book
+
+app.get('/book/:id', (req, res)=> {
+   Book.findById(req.params.id).then((book) => {
+      if (book){
+         res.json(book)
+      }else {
+         res.sendStatus(404);
+      }
+   }).catch(err => {
+      if(err){
+         throw err;
+      }
+   })
+})
+//delete book
+app.delete('/book/:id', (req, res)=> {
+   Book.findOneAndRemove(req.params.id).then(() => {
+      res.send("Book deleted successfully")
+      
+   }).catch(err => {
+      if(err){
+         throw err;
+      }
+   })
+})
+
+
+
+
 app.listen(4000, ()=> {
    console.log('Book service is up and running on port 4000')
 });
